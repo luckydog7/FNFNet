76871d3cfabd61280edad2ee79fb0faf3324a89d
Dominator98 <kirkj@goatse.info>
2021-06-14 10:37:10 +0200

    abnormally large

--- src/rooms/schema/Stuff.ts
+++ src/rooms/schema/Stuff.ts
@@ -11,6 +11,8 @@ export class Stuff extends Schema {
 
   @type("string") start: string;
 
+  @type("string") creatematch: string;
+
   @type('int32') retnumber: number;
 
   @type("int32") axY: Number = 0;
